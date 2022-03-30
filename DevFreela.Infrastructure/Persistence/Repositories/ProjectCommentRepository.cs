307809ddb4d3fa00d242e3e197fc0e2ff6e4abbd
renan <renan@interprocess.com.br>
2022-03-30 17:52:00 -0300

    feat: add repositories

---
+++ DevFreela.Infrastructure/Persistence/Repositories/ProjectCommentRepository.cs
@@ -0,0 +1,27 @@
+﻿using DevFreela.Core.Entities;
+using DevFreela.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Infrastructure.Persistence.Repositories
+{
+    public class ProjectCommentRepository : IProjectCommentRepository
+    {
+        private readonly DevFreelaDbContext _dbContext;
+
+        public ProjectCommentRepository(DevFreelaDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task CreateCommentAsync(ProjectComment comment)
+        {
+            await _dbContext.ProjectComments.AddAsync(comment);
+
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ DevFreela.Infrastructure/Persistence/Repositories/SkillRepository.cs
@@ -0,0 +1,24 @@
+﻿using DevFreela.Core.Entities;
+using DevFreela.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DevFreela.Infrastructure.Persistence.Repositories
+{
+    public class SkillRepository : ISkillRepository
+    {
+        private readonly DevFreelaDbContext _dbContext;
+
+        public SkillRepository(DevFreelaDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Skill>> GetallSkillsAsync()
+        {
+            return await _dbContext.Skills.ToListAsync();
+        }
+
+    }
+}
