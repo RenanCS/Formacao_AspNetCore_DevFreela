26f1e5e8517f27978ef3a20c83d45bded0ff0e7f
renan <renan@interprocess.com.br>
2022-03-30 17:51:47 -0300

    feat: add repositories

--- DevFreela.Core/Repositories/IProjectRepository.cs
+++ DevFreela.Core/Repositories/IProjectRepository.cs
@@ -1,7 +1,17 @@
 ﻿
+using DevFreela.Core.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
 namespace DevFreela.Core.Repositories
 {
     public interface IProjectRepository
     {
+        public Task<List<Project>> GetAllProjectsAsync();
+
+        public Task<Project> GetProjectByIdAsync(int id);
+        public Task CreateProjectAsync(Project project);
+
+        public Task DeleteProjectAsync(int id);
     }
 }
---
+++ DevFreela.Core/Repositories/ISkillRepository.cs
@@ -0,0 +1,11 @@
+﻿using DevFreela.Core.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DevFreela.Core.Repositories
+{
+    public interface ISkillRepository
+    {
+        public Task<List<Skill>> GetallSkillsAsync();
+    }
+}
