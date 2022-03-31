7f408c7f51b3f59cc2b2f7ccd75d4c165a2d4efc
renan <renan@interprocess.com.br>
2022-03-31 14:12:55 -0300

    chore: remove reference unnecessary

--- DevFreela.Infrastructure/Persistence/Repositories/ProjectCommentRepository.cs
+++ DevFreela.Infrastructure/Persistence/Repositories/ProjectCommentRepository.cs
@@ -1,9 +1,5 @@
 ﻿using DevFreela.Core.Entities;
 using DevFreela.Core.Repositories;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace DevFreela.Infrastructure.Persistence.Repositories
