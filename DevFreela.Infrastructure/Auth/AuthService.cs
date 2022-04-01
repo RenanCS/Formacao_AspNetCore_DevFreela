9741075bcc18765722f53ba397ab8101b02ec23f
renan <renan@interprocess.com.br>
2022-04-01 18:07:24 -0300

    feat: create class

---
+++ DevFreela.Infrastructure/Auth/AuthService.cs
@@ -0,0 +1,55 @@
+﻿using DevFreela.Core.Service;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace DevFreela.Infrastructure.Auth
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IConfiguration _configuration;
+        public AuthService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateJWTToken(string email, string role)
+        {
+            var issuer = _configuration["Jwt:Issuer"];
+            var audience = _configuration["Jwt:Audience"];
+            var key = _configuration["Jwt:Key"];
+            var expires = _configuration["Jwt:Expires"];
+
+            var keyBytes = Encoding.UTF8.GetBytes(key);
+
+            var securityKey = new SymmetricSecurityKey(keyBytes);
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new List<Claim>();
+            claims.Add(new Claim("userName", email));
+            claims.Add(new Claim(ClaimTypes.Role, role));
+
+
+            var token = new JwtSecurityToken(
+                issuer: issuer,
+                audience: audience,
+                expires: DateTime.Now.AddHours(int.Parse(expires)),
+                signingCredentials: credentials,
+                claims: claims);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var stringToken = tokenHandler.WriteToken(token);
+
+            return stringToken;
+
+
+
+
+        }
+    }
+}
