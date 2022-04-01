264d4a0e82ef68142b802471a4a95aa66ea9c44c
renan <renan@interprocess.com.br>
2022-04-01 18:23:34 -0300

    feat: add method encryption for password

--- DevFreela.Infrastructure/Auth/AuthService.cs
+++ DevFreela.Infrastructure/Auth/AuthService.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace DevFreela.Infrastructure.Auth
@@ -46,10 +47,22 @@ namespace DevFreela.Infrastructure.Auth
             var stringToken = tokenHandler.WriteToken(token);
 
             return stringToken;
+        }
 
+        public string ComputeSha256Hash(string password)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
 
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
 
-
+                return builder.ToString();
+            }
         }
     }
 }
