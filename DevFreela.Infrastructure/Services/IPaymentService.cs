c1f3097656643ecfde6dde67c93e8c70b8ffcb3c
renan <renan@interprocess.com.br>
2022-04-13 09:13:19 -0300

    feat: add payment service

---
+++ DevFreela.Infrastructure/Services/IPaymentService.cs
@@ -0,0 +1,10 @@
+﻿using DevFreela.Core.DTO;
+using System.Threading.Tasks;
+
+namespace DevFreela.Infrastructure.Services
+{
+    public interface IPaymentService
+    {
+        Task<bool> ProcessPayment(PaymentInfoDTO paymentInfoDTO);
+    }
+}
